65d1f3f78200ae246b6c7110536db67963586e5e
julglad <48397479+julglad@users.noreply.github.com>
2021-02-22 12:34:07 +0700

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -20,7 +20,7 @@ rocketlist = ["all", "falcon1","falcon9","falconheavy"]
 for i in range(4):
     t1 = BashOperator(
         task_id="get_data" + str(i), 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r " + rocketlist[i]", 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r " + rocketlist[i], 
         dag=dag
     )
 
