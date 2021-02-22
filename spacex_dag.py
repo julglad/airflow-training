d4ab442f65dc9b7fd0437b6ba73f57afdce3ef29
julglad <48397479+julglad@users.noreply.github.com>
2021-02-22 10:22:31 +0700

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -24,7 +24,7 @@ t1 = BashOperator(
 
 t2 = BashOperator(
     task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ myrocket }}/data.csv", 
+    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
     params={"rocket": "falcon1"}, # falcon1/falcon9/falconheavy
     dag=dag
 )
