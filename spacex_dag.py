203d1d63816756ae7eb792c6ff7e6ac76d1104a8
julglad <48397479+julglad@users.noreply.github.com>
2021-02-23 13:26:33 +0700

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -16,7 +16,7 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 rocketlist = ["all", "falcon1","falcon9","falconheavy"]
-r = f" -r {myrocket}" if myrocket != 'all' else ""
+r = " -r " + myrocket if myrocket != 'all' else ""
 for myrocket in rocketlist:
     t1 = BashOperator(
         task_id="get_data_" + myrocket, 
