cbcbfbb2132c9059b7221fe652d57160938d1885
julglad <48397479+julglad@users.noreply.github.com>
2021-02-23 15:02:37 +0700

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -16,10 +16,14 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 rocketlist = ["all", "falcon1","falcon9","falconheavy"]
-for myrocket in rocketlist: 
+for myrocket in rocketlist:
+    if myrocket = "all":
+        r = ""
+    else:
+        r =  " -r " + myrocket
     t1 = BashOperator(
         task_id="get_data_" + myrocket, 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r " + myrocket,
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data" + r,
         dag=dag
     )
 
