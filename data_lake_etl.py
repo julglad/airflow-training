60bb0fc88358db2c8570a4e7431ae22c9d6bf97c
julglad <48397479+julglad@users.noreply.github.com>
2021-04-10 21:51:11 +0700

    Update data_lake_etl.py

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -18,15 +18,39 @@ dag = DAG(
     schedule_interval="0 0 1 1 *",
 )
 
-ods_billing = DataProcHiveOperator(
-    task_id='ods_billing',
+tasks  = {'billing': 'created_at',
+          'issue': 'start_time',
+          'payment': 'pay_date',
+          'traffic': 'from_unixtime(`timestamp` DIV 1000)'}
+
+ods=[]
+for task, partcolumn in tasks.items():
+    query = 'insert overwrite table ygladkikh.ods_' + task + " partition (year='{{ execution_date.year }}') " \
+            'select * from ygladkikh.stg_'+ task + ' where year(' + partcolumn + ') = {{ execution_date.year }};'
+    ods.append(DataProcHiveOperator(
+        task_id='ods'+task,
+        dag=dag,
+        query=query,
+        cluster_name='cluster-dataproc',
+        job_name=USERNAME + '_ods_'+task+'_{{ execution_date.year }}_{{ params.job_suffix }}',
+        params={"job_suffix": randint(0, 100000)},
+        region='europe-west3',
+    ))
+
+dm_traffic= DataProcHiveOperator(
+    task_id='dm_traffic',
     dag=dag,
     query="""
-        insert overwrite table ygladkikh.ods_billing partition (year='{{ execution_date.year }}') 
-        select * from ygladkikh.stg_billing where year(created_at) = {{ execution_date.year }};
+        insert overwrite table ygladkikh.dm_bytes_received partition (year='{{ execution_date.year }}') 
+        select user_id, max(bytes_received), min(bytes_received), ceil(avg(bytes_received)) 
+        from ygladkikh.ods_traffic 
+        group by user_id
+        where year(created_at) = {{ execution_date.year }};
     """,
     cluster_name='cluster-dataproc',
-    job_name=USERNAME + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
+    job_name=USERNAME + '_dm_traffic_{{ execution_date.year }}_{{ params.job_suffix }}',
     params={"job_suffix": randint(0, 100000)},
     region='europe-west3',
 )
+
+ods >> dm_traffic
