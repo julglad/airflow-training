f415269cc27a28945338e51878d7b7a62c093407
julglad <48397479+julglad@users.noreply.github.com>
2021-04-11 17:55:28 +0700

    Update data_lake_etl.py

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -19,18 +19,6 @@ dag = DAG(
     schedule_interval="0 0 1 1 *",
 )
 
-ods_traffic = DataProcHiveOperator(
-    task_id='ods_traffic',
-    dag=dag,
-    query="""
-        insert overwrite table ygladkikh.ods_traffic partition (year='{{ execution_date.year }}') 
-        select * from ygladkikh.stg_traffic where year(from_unixtime(`timestamp` DIV 1000)) = {{ execution_date.year }};
-    """,
-    cluster_name='cluster-dataproc',
-    job_name=USERNAME + '_ods_traffic_{{ execution_date.year }}_{{ params.job_suffix }}',
-    params={"job_suffix": randint(0, 100000)},
-    region='europe-west3',
-)
 ods_issue = DataProcHiveOperator(
     task_id='ods_issue',
     dag=dag,
