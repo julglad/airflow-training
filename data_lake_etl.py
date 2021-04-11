b736946891fa461e52d3dd09311af4e416536f3c
julglad <48397479+julglad@users.noreply.github.com>
2021-04-11 12:05:55 +0700

    Update data_lake_etl.py

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -18,18 +18,34 @@ dag = DAG(
     schedule_interval="0 0 1 1 *",
 )
 
-tasks  = {'billing': 'cast(created_at as date)',
-          'issue': 'cast(start_time as date)',
-          'payment': 'cast(pay_date as date)',
-          'traffic': 'from_unixtime(`timestamp` DIV 1000)'}
+tasks = {'billing': '''
+                    insert overwrite table ygladkikh.ods_billing partition (year='{{ execution_date.year }}') 
+                    select cast(user_id as BIGINT), billing_period, service, tariff, cast(sum as DECIMAL(10,2)), cast(created_at as TIMESTAMP)  
+                    from ygladkikh.stg_billing where year(cast(created_at as date)) = {{ execution_date.year }};
+        ''',
+         'issue': '''
+                    insert overwrite table ygladkikh.ods_issue partition (year='{{ execution_date.year }}')                    
+                    select cast(user_id as BIGINT), cast(start_time as TIMESTAMP), cast(end_time as TIMESTAMP), title, description, service 
+                    from ygladkikh.stg_issue where year(cast(start_time as date)) = {{ execution_date.year }};         
+          ''',
+         'payment': '''
+                    insert overwrite table ygladkikh.ods_payment partition (year='{{ execution_date.year }}')                    
+                    select cast(user_id as BIGINT), pay_doc_type, cast(pay_doc_num as BIGINT), account, phone, billing_period, cast(pay_date as TIMESTAMP), cast(sum as DECIMAL(10,2)) 
+                    from ygladkikh.stg_payment where year(cast(pay_date as date)) = {{ execution_date.year }};           
+          ''',
+         'traffic': '''
+                    insert overwrite table ygladkikh.ods_traffic partition (year='{{ execution_date.year }}')                    
+                    select * 
+                    from ygladkikh.stg_traffic where year(from_unixtime(`timestamp` DIV 1000)) = {{ execution_date.year }};            
+          '''}
 
-dm_traffic= DataProcHiveOperator(
+dm_traffic = DataProcHiveOperator(
     task_id='dm_traffic',
     dag=dag,
     query="""
         insert overwrite table ygladkikh.dm_traffic partition (year='{{ execution_date.year }}') 
         select user_id, max(bytes_received), min(bytes_received), ceil(avg(bytes_received)) 
-        from ygladkikh.ods_traffic         
+        from ygladkikh.ods_traffic 
         where year(created_at) = {{ execution_date.year }}
         group by user_id;
     """,
@@ -39,14 +55,13 @@ dm_traffic= DataProcHiveOperator(
     region='europe-west3',
 )
 
-for task, partcolumn in tasks.items():
-    query = 'insert overwrite table ygladkikh.ods_' + task + " partition (year='{{ execution_date.year }}') select * from ygladkikh.stg_"+ task + ' where year(' + partcolumn + ') = {{ execution_date.year }};'
+for task, query in tasks.items():   
     ods = DataProcHiveOperator(
-        task_id='ods_'+task,
+        task_id='ods_' + task,
         dag=dag,
         query=query,
         cluster_name='cluster-dataproc',
-        job_name=USERNAME + '_ods_'+task+'_{{ execution_date.year }}_{{ params.job_suffix }}',
+        job_name=USERNAME + '_ods_' + task + '_{{ execution_date.year }}_{{ params.job_suffix }}',
         params={"job_suffix": randint(0, 100000)},
         region='europe-west3',
     )
