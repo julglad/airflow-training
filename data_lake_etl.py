9177a35014758de678051b10b2f1193b24711dcf
julglad <48397479+julglad@users.noreply.github.com>
2021-04-11 13:35:22 +0700

    Update data_lake_etl.py

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -39,31 +39,45 @@ tasks = {'billing': '''
                     from ygladkikh.stg_traffic where year(from_unixtime(`timestamp` DIV 1000)) = {{ execution_date.year }};            
           '''}
 
-dm_traffic = DataProcHiveOperator(
-    task_id='dm_traffic',
+# dm_traffic = DataProcHiveOperator(
+#     task_id='dm_traffic',
+#     dag=dag,
+#     query="""
+#         insert overwrite table ygladkikh.dm_traffic partition (year='{{ execution_date.year }}') 
+#         select user_id, max(bytes_received), min(bytes_received), ceil(avg(bytes_received)) 
+#         from ygladkikh.ods_traffic 
+#         where year(created_at) = {{ execution_date.year }}
+#         group by user_id;
+#     """,
+#     cluster_name='cluster-dataproc',
+#     job_name=USERNAME + '_dm_traffic_{{ execution_date.year }}_{{ params.job_suffix }}',
+#     params={"job_suffix": randint(0, 100000)},
+#     region='europe-west3',
+# )
+
+# for task, query in tasks.items():
+#     ods = DataProcHiveOperator(
+#         task_id='ods_' + task,
+#         dag=dag,
+#         query=query,
+#         cluster_name='cluster-dataproc',
+#         job_name=USERNAME + '_ods_' + task + '_{{ execution_date.year }}_{{ params.job_suffix }}',
+#         params={"job_suffix": randint(0, 100000)},
+#         region='europe-west3',
+#     )
+#     if task == 'traffic':
+#         ods >> dm_traffic
+
+ods_billing = DataProcHiveOperator(
+    task_id='ods_billing',
     dag=dag,
     query="""
-        insert overwrite table ygladkikh.dm_traffic partition (year='{{ execution_date.year }}') 
-        select user_id, max(bytes_received), min(bytes_received), ceil(avg(bytes_received)) 
-        from ygladkikh.ods_traffic 
-        where year(created_at) = {{ execution_date.year }}
-        group by user_id;
-    """,
+         insert overwrite table ygladkikh.ods_billing partition (year='{{ execution_date.year }}') 
+                            select cast(user_id as BIGINT), billing_period, service, tariff, cast(sum as DECIMAL(10,2)), cast(created_at as TIMESTAMP)  
+                            from ygladkikh.stg_billing where year(cast(created_at as date)) = {{ execution_date.year }};
+            """,
     cluster_name='cluster-dataproc',
-    job_name=USERNAME + '_dm_traffic_{{ execution_date.year }}_{{ params.job_suffix }}',
+    job_name=USERNAME + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
     params={"job_suffix": randint(0, 100000)},
     region='europe-west3',
 )
-
-for task, query in tasks.items():   
-    ods = DataProcHiveOperator(
-        task_id='ods_' + task,
-        dag=dag,
-        query=query,
-        cluster_name='cluster-dataproc',
-        job_name=USERNAME + '_ods_' + task + '_{{ execution_date.year }}_{{ params.job_suffix }}',
-        params={"job_suffix": randint(0, 100000)},
-        region='europe-west3',
-    )
-    if task == 'traffic':
-        ods >> dm_traffic
