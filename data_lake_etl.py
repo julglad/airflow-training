e5f566f716ce1f7cb758bd5994ff67faa42e589b
julglad <48397479+julglad@users.noreply.github.com>
2021-04-11 17:56:36 +0700

    Update data_lake_etl.py

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -23,7 +23,7 @@ ods_issue = DataProcHiveOperator(
     task_id='ods_issue',
     dag=dag,
     query="""
-        insert overwrite table ygladkikh.ods_issue partition (year='{{ execution_date.year }}') select cast(user_id as BIGINT), cast(start_time as TIMESTAMP), cast(end_time as TIMESTAMP), title, description, service from ygladkikh.stg_issue where year(cast(start_time  AS TIMESTAMP) = {{ execution_date.year }};
+        insert overwrite table ygladkikh.ods_issue partition (year='{{ execution_date.year }}') select cast(user_id as BIGINT), cast(start_time as TIMESTAMP), cast(end_time as TIMESTAMP), title, description, service from ygladkikh.stg_issue where year(cast(start_time  AS TIMESTAMP)) = {{ execution_date.year }};
     """,
     cluster_name='cluster-dataproc',
     job_name=USERNAME + '_ods_issue_{{ execution_date.year }}_{{ params.job_suffix }}',
