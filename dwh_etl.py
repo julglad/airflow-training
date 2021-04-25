beea58e5f10967faa254d8aa01ce2f2d077c0fe2
julglad <48397479+julglad@users.noreply.github.com>
2021-04-25 11:46:48 +0700

    Update dwh_etl.py

--- dwh_etl.py
+++ dwh_etl.py
@@ -33,7 +33,7 @@ fill_ods = PostgresOperator(
     dag=dag,
     sql="""
         INSERT INTO ygladkikh.ods_payment(user_id,pay_doc_type,pay_doc_num,account,phone,billing_period,pay_date,sum)
-        SELECT user_id,pay_doc_type text,pay_doc_num,account,phone,billing_period::DATE,pay_date::DATE,sum FROM ygladkikh.stg_payment 
+        SELECT user_id,pay_doc_type,pay_doc_num,account,phone,billing_period::DATE,pay_date::DATE,sum FROM ygladkikh.stg_payment 
         WHERE EXTRACT(YEAR FROM pay_date::DATE) = {{ execution_date.year }}
     """
 )
